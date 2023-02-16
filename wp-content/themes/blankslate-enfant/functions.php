dc0981f82ca3fdafbfa6dd17ae469fa936d3ebdb
Rnygtr <116190095+Rnygtr@users.noreply.github.com>
2023-02-16 13:23:36 +0100

    fin hook

--- wp-content/themes/blankslate-enfant/functions.php
+++ wp-content/themes/blankslate-enfant/functions.php
@@ -8,3 +8,12 @@ function theme_enqueue_styles() {
 
 }
 
+function add_admin_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in()) {
+        $items .= '<li id="menu-item-285" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-285"><a href="http://localhost/planty/admin/" itemprop="url"><span itemprop="name">Admin</span></a></li>';
+    }
+    return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'add_admin_item_to_nav_menu', 10, 2);
+
