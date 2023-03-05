6cebef89fffe7fd1ee53e7c439d60972a960042e
Rnygtr <116190095+Rnygtr@users.noreply.github.com>
2023-03-05 06:57:24 +0100

    fin correction

--- wp-content/themes/blankslate-enfant/header.php
+++ wp-content/themes/blankslate-enfant/header.php
@@ -11,14 +11,14 @@
 <header id="header" role="banner">
 <div class="flex-between">
 
-   <a href="http://localhost/planty/" ><img class="logo" src="/planty/wp-content/uploads/2023/02/Logo.png" alt="logo planty" width="190px" height="20px"> </a>
+   <a href="http://localhost/planty/" ><img class="logo" src="/planty/wp-content/uploads/2023/02/Logo.png" alt="logo planty" > </a>
 
 
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
 
-<a class="commander" href="http://localhost/planty/commande/">Commander</a>
+
 </div>
-</nav>
+
 </header>
 <div id="container">
 <main id="content" role="main">
